33b0e96ca80095d26b3ed73b2b7a5a92eee0cab2
faiyyazs <faiyyazshaikh2010@gmail.com>
2026-06-25 16:39:44 +0530

    Switch to HashRouter to fix GitHub Pages 404s

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MainSite from './MainSite';
 import AdminPortal from './AdminPortal';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<MainSite />} />
         <Route path="/salt" element={<AdminPortal />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
